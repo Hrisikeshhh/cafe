73080ed28eb49a282ec078a088afcf11044c7298
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-07-23 18:20:45 +0530

    modify

---
+++ index.js
@@ -0,0 +1,100 @@
+ document.addEventListener("DOMContentLoaded",()=>{
+
+
+ 
+   var swiper1 = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay:{
+         delay: 3500,
+         disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+// reviews
+
+     var swiperTwo = new Swiper(".mySwiperTwo", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        autoplay:{
+        delay: 2500,
+        disableOnInteraction: false,
+        },
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
+
+
+      let menuBtn = document.querySelector("#menu");
+
+      menuBtn.addEventListener("click",()=>{
+        setTimeout(()=>{
+          swiper1.update();
+        },300);
+      })
+
+
+      // for mobile screen
+      let mm = gsap.matchMedia();
+
+      mm.add("(max-width: 800px)",()=>{
+        
+       let menu = document.querySelector("#menu"); 
+       let close = document.querySelector("#close");
+      let tlOne = gsap.timeline()
+
+      tlOne.to(".listItems",{
+        x:355,
+        duration:0.7
+      })
+
+      tlOne.from(".items",{
+        x:40,
+        duration:0.4,
+        stagger:0.4,
+        opacity:0
+      })
+
+      tlOne.from("#close",{
+        scale:0,
+        duration:0.5,
+        opacity:0
+      })
+
+      tlOne.pause();
+           
+     menu.addEventListener("click",()=>{
+      tlOne.play();
+     })
+
+     close.addEventListener("click",()=>{
+      tlOne.reverse();
+     })
+
+      })
+
+
+
+
+
+
+    
+    })
