4bf5ba4d6ce8aaa927039dd19db193c3c560eaa8
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-08-06 13:52:29 +0530

    add responsive

--- aboutEmpolyee.js
+++ aboutEmpolyee.js
@@ -1,5 +1,4 @@
 
-
 fetch("aboutEmployee.json")
 .then((response)=>{
  return response.json();
@@ -23,3 +22,43 @@ fetch("aboutEmployee.json")
   }
 
 })
+
+
+// for mobile screen
+      let mm = gsap.matchMedia();
+
+      mm.add("(max-width: 800px)",()=>{
+        
+       let menu = document.querySelector("#menu"); 
+       let close = document.querySelector("#close");
+      let tlOne = gsap.timeline()
+
+      tlOne.to(".listItems",{
+        x:355,
+        duration:0.7
+      })
+
+      tlOne.from(".items",{
+        x:40,
+        duration:0.4,
+        stagger:0.4,
+        opacity:0
+      })
+
+      tlOne.from("#close",{
+        scale:0,
+        duration:0.5,
+        opacity:0
+      })
+
+      tlOne.pause();
+           
+     menu.addEventListener("click",()=>{
+      tlOne.play();
+     })
+
+     close.addEventListener("click",()=>{
+      tlOne.reverse();
+     })
+
+    });
