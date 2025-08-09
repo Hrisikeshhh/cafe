54cd1a24957d126ca847b73aa526836ace98b7b8
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-08-09 11:06:47 +0530

    add

--- menu.js
+++ menu.js
@@ -1,10 +1,3 @@
-let mapBtn = document.querySelector("#see");
-
-mapBtn.addEventListener("click",()=>{
-    window.location.href = "https://www.google.com/maps/place/Hotel+Brindavan/@26.995102,94.64434,15z/data=!4m9!3m8!1s0x374731532500d8ab:0x77ffdbcda9323c7e!5m2!4m1!1i2!8m2!3d26.9951019!4d94.6443395!16s%2Fg%2F11cs6g_r8s?hl=en&entry=ttu&g_ep=EgoyMDI1MDczMC4wIKXMDSoASAFQAw%3D%3D";
-})
-
-
  // for mobile screen
       let mm = gsap.matchMedia();
 
@@ -44,4 +37,15 @@ mapBtn.addEventListener("click",()=>{
 
 
       })
+
+
+let ar = document.querySelectorAll(".p");
+
+ar.forEach((e)=>{
+   e.addEventListener("click",()=>{
+     localStorage.setItem("productId",
+        e.dataset.id )
+    window.location.href = "productDetail.html";
+   })
+})
 
--- productDetail.js
+++ productDetail.js
@@ -37,6 +37,48 @@ let productPage = document.querySelectorAll(".p");
      });
 
 
+     
+
+    //  / for mobile screen
+      let mm = gsap.matchMedia();
+
+      mm.add("(max-width: 800px)",()=>{
+        
+       let menu = document.querySelector("#menu"); 
+       let close = document.querySelector("#close");
+      let tlOne = gsap.timeline()
+
+      tlOne.to(".listItems",{
+        x:355,
+        duration:0.7
+      })
+
+      tlOne.from(".items",{
+        x:40,
+        duration:0.4,
+        stagger:0.4,
+        opacity:0
+      })
+
+      tlOne.from("#close",{
+        scale:0,
+        duration:0.5,
+        opacity:0
+      })
+
+      tlOne.pause();
+           
+     menu.addEventListener("click",()=>{
+      tlOne.play();
+     })
+
+     close.addEventListener("click",()=>{
+      tlOne.reverse();
+     })
+
+    });
+
+
 
 
 
